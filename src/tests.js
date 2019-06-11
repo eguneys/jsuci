fc967602a53d01764ba2127042950cdc72a62569
Emre Guneyler <eguneys@hotmail.com>
2019-06-11 21:49:04 +0000

    dispatch command

--- src/tests.js
+++ src/tests.js
@@ -1,9 +1,32 @@
 const testutils = require('testiz');
 const { log, ok, deep_is, is, not, throws } = testutils;
 
-const { parseCommand } = require('./uci');
+const Uciengine = require('./uci');
+
+function withDispatchEvent(command, event, f) {
+  var called;
+  var { parseCommand, dispatchCommand } = Uciengine({
+    events: {
+      [event]: (...args) => {
+        called = true;
+        f(...args);
+      }
+    }
+  });
+
+  var res = dispatchCommand(parseCommand(command));
+
+  if (!called) {
+    throw new Error("didn't fire " + event);
+  }
+  
+  return res;
+}
 
 function tests() {
+  
+  var { parseCommand, dispatchCommand } = Uciengine({});
+
   log('parse command');
 
   var bads = [
@@ -28,8 +51,37 @@ function tests() {
 
   goods.forEach(good =>
     deep_is("command " + good[0], parseCommand(good[0]), good[1]));
-    
 
+
+  log('dispatch command');
+
+  throws("empty command", "Unknown command: ", () => dispatchCommand(parseCommand("")));
+
+  is("quit returns false", false, dispatchCommand(parseCommand("quit")));
+
+  var dispatches = [
+    { command: "uci", event: "uci", callback: [] },
+    { command: "isready", event: "isready", callback: [] },
+    { command: "setoption", event: "setoption", callback: [{ name: "", value: "", context: ""}] },
+    { command: "setoption name x y z value a b", event: "setoption", callback: [{ name: "x y z", value: "a b", context: ""}] },
+    { command: "ucinewgame", event: "ucinewgame", callback: [] },
+    { command: "position", event: "position", callback: [{ fen: "", startpos: "", moves: "" }] },
+    { command: "position fen 1/2/3 w k", event: "position", callback: [{ fen: "1/2/3 w k", startpos: "", moves: "" }] },
+    { command: "go", event: "go", callback: [{}] },
+    { command: "go infinite", event: "go", callback: [{ infinite: true }] },
+    { command: "go ponder", event: "go", callback: [{ ponder: true }] },
+    { command: "go searchmoves abc d", event: "go", callback: [{searchmoves: "abc d"}] },
+    { command: "go wtime 3", event: "go", callback: [{ wtime: 3 }]},
+    { command: "start", event: "start", callback: [] },
+    { command: "stop", event: "stop", callback: [] },
+    { command: "ponderhit", event: "ponderhit", callback: [] }
+  ];
+
+  dispatches.forEach(dispatch => 
+    is(dispatch.command + " is ok", true, withDispatchEvent(dispatch.command, dispatch.event, (...args) => {
+      deep_is(dispatch.event + " fired", args, dispatch.callback);
+    })));
+  
 }
 
 tests();
--- src/uci.js
+++ src/uci.js
@@ -1,88 +1,162 @@
 const readline = require('readline');
 
-const knownCommands = {
-  "uci": [],
-  "isready": [],
-  "setoption": ["context", "name", "value"],
-  "ucinewgame": [],
-  "position": ["fen", "startpos", "moves"],
-  "go": ["infinite", "wtime", "btime", "winc", "binc", "movestogo", "depth", "nodes", "movetime", "searchmoves", "ponder"],
-  "start": [],
-  "stop": [],
-  "ponderhit": [],
-  "quit": [],
-  "xyzzy": []
+function callUserFunction(f, ...args) {
+  if (f) f(...args);
+}
+
+const defaults = {
+  events: {}
 };
 
-function parseCommand(line) {
-  const tokens = line.split(' ');
+module.exports = function Uciengine(opts = defaults) {
 
-  var token = tokens[0],
-      params = {},
-      value,
-      iter,
-      whitespace;
+  const knownCommands = {
+    "uci": [],
+    "isready": [],
+    "setoption": ["context", "name", "value"],
+    "ucinewgame": [],
+    "position": ["fen", "startpos", "moves"],
+    "go": ["infinite", "wtime", "btime", "winc", "binc", "movestogo", "depth", "nodes", "movetime", "searchmoves", "ponder"],
+    "start": [],
+    "stop": [],
+    "ponderhit": [],
+    "quit": [],
+    "xyzzy": []
+  };
 
-  if (!token) {
-    return {};
-  }
+  function parseCommand(line) {
+    const tokens = line.split(' ');
 
-  const command = { token, value: knownCommands[token] };
-  if (!command.value) {
-    throw new Error("Unknown command: " + line);
-  }
+    var token = tokens[0],
+        params = {},
+        value,
+        iter,
+        whitespace;
 
-  for (var i = 1; i < tokens.length; i++) {
-    token = tokens[i];
-    if (command.value.indexOf(token) === -1) {
-      if (!value) throw new Error("Unexpected token: " + token);
-      params[value.addr] += whitespace + token;
-      whitespace = " ";
-    } else {
-      value = { addr: token, value: "" };
-      params[value.addr] = value.value;
-      whitespace = "";
+    if (!token) {
+      return {};
     }
-  }
 
-  return { first: command.token, params };
-}
+    const command = { token, value: knownCommands[token] };
+    if (!command.value) {
+      throw new Error("Unknown command: " + line);
+    }
 
-function dispatchCommand(command) {
-  switch (command.first) {
-  case "quit":
-    return false;
-  default: 
-    return true;
-  }
-};
-
-function sendResponse(response) {
-  console.log(response);
-}
-
-function runloop() {
-  const rl = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout
-  });
-
-  rl.on('line', line => {
-
-    try {
-      const command = parseCommand(line);
-
-      if (command) {
-        if (!dispatchCommand(command)) rl.close();
+    for (var i = 1; i < tokens.length; i++) {
+      token = tokens[i];
+      if (command.value.indexOf(token) === -1) {
+        if (!value) throw new Error("Unexpected token: " + token);
+        params[value.addr] += whitespace + token;
+        whitespace = " ";
+      } else {
+        value = { addr: token, value: "" };
+        params[value.addr] = value.value;
+        whitespace = "";
       }
-    } catch(err) {
-      sendResponse("error " + err);
     }
-  });
-}
 
-module.exports = {
-  parseCommand,
-  dispatchCommand,
-  runloop
+    return { first: command.token, params };
+  }
+
+  function dispatchCommand(command) {
+    function goOption(x, params, goParams) {
+      if (params[x]) {
+        goParams[x] = parseInt(params[x]);
+      }
+    }
+
+    switch (command.first) {
+    case "uci":
+      callUserFunction(opts.events.uci);
+      break;
+    case "isready":
+      callUserFunction(opts.events.isready);
+      break;
+    case "setoption":
+      callUserFunction(opts.events.setoption, {
+        name: "", value: "", context: "", ...command.params
+      });
+      break;
+    case "ucinewgame":
+      callUserFunction(opts.events.ucinewgame);
+      break;
+    case "position":
+      callUserFunction(opts.events.position, {
+        fen: "", startpos: "", moves: "", ...command.params
+      });
+      break;
+    case "go":
+      var goParams = {};
+
+      if (command.params.infinite === "") {
+        goParams.infinite = true;
+      }
+      if (command.params.searchmoves) {
+        goParams.searchmoves = command.params.searchmoves;
+      }
+      if (command.params.ponder === "") {
+        goParams.ponder = true;
+      }
+
+      ['wtime',
+       'btime',
+       'winc',
+       'binc',
+       'movestogo',
+       'depth',
+       'nodes',
+       'movetime'].forEach(opt =>
+         goOption(opt, command.params, goParams));
+
+      callUserFunction(opts.events.go, goParams);
+      break;
+    case "stop":
+      callUserFunction(opts.events.stop);
+      break;
+    case "ponderhit":
+      callUserFunction(opts.events.ponderhit);
+      break;
+    case "start":
+      callUserFunction(opts.events.start);
+      break;
+    case "xyzzy":
+      sendResponse("Nothing happens.");
+      break;
+    case "quit":
+      return false;
+    default: 
+      throw new Error("Unknown command: " + command.first);
+    }
+    return true;
+  };
+
+  function sendResponse(response) {
+    console.log(response);
+  }
+
+  function runloop() {
+    const rl = readline.createInterface({
+      input: process.stdin,
+      output: process.stdout
+    });
+
+    rl.on('line', line => {
+
+      try {
+        const command = parseCommand(line);
+
+        if (command) {
+          if (!dispatchCommand(command)) rl.close();
+        }
+      } catch(err) {
+        sendResponse("error " + err);
+      }
+    });
+  }
+
+  return {
+    parseCommand,
+    dispatchCommand,
+    runloop,
+  };
 };
